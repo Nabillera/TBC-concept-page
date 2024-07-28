e2fe880c74d119e66988c9ead54aa9ad17c910f4
Nabillera <133674245+Nabillera@users.noreply.github.com>
2024-07-28 22:52:55 +0400

    app download v2

--- components/app-download.js
+++ components/app-download.js
@@ -12,18 +12,18 @@ export class AppDownload extends HTMLElement {
           <h2 id="digital-bank-heading">ციფრული ბანკი</h2>
           <div id="illustration-wrapper"><img id="app-illustration" src="https://static.tbcconcept.ge/mobile/media/3sdx4uin.3qibankkk3.png"/></div>
           <p id="app-description">შეასრულეთ საბანკო ოპერაციები დისტანციურად, ციფრული ბანკის დახმარებით</p>
-          <a id="app-download-button">
+          <a id="app-download-button" href="https://play.google.com/store/apps/details?id=com.icomvision.bsc.tbc&hl=en">
               <div id="app-download-button-content">
 								<img src="../images/download-icon.svg"/>
 								<span>DOWNLOAD</span>
               </div>
           </a>
           <div id="download-options">
-            <a><div class="download-option-button">
+            <a href="https://play.google.com/store/apps/details?id=com.icomvision.bsc.tbc&hl=en"><div class="download-option-button">
               <img src="../images/google-play.svg"/>
             </div></a>
 
-            <a><div class="download-option-button">
+            <a href="https://apps.apple.com/us/app/tbc-bank/id766598432"><div class="download-option-button">
               <img src="../images/app-store.svg"/>
             </div></a>
           </div>
